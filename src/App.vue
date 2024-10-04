06361f4e059b0868ce17a85daf4e0a7ff4ddbe56
federicoverbicaro <federico.verbicaro@icloud.com>
2024-10-04 16:27:58 +0200

    img time

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,23 @@
 </script>
 
 <template>
-  <div class="h-screen">
+  <div class="h-screen bg-purple-300">
     <div class="text-center w-full p-5">
       <h1 class="text-red-500 text-4xl">Time Pomodoro</h1>
     </div>
 
     <div class="flex h-full justify-center items-center">
-      <div class="border border-black w-1/2 h-96 flex justify-center items-center">
-        <img class="h-80 w-80" src="../public/OIG4.jpeg" alt="">
+      <div class=" w-1/2 h-96 flex">
+        <div class=" w-1/2" >
+          <h1 class="text-4xl capitalize text-red-500 p-6">click me</h1>
+          <div class="mt-3 flex justify-end">
+            <img class="h-56 w-56" src="../public/086aa12b1847d74942b5dba9972d2e7d-Photoroom.png" alt="">
+          </div>
+        </div>
+        <div class=" w-1/2 flex justify-center items-center">
+          <img class="h-80 w-80" src="../public/OIG4-Photoroom.png" alt="">
+        </div>
       </div>
-
       <div class="border border-black w-1/2 h-96 flex justify-center items-center">
         <div class="border border-black rounded-full h-96 w-96 flex justify-center items-center">
           <span class="text-4xl">3:07</span>
