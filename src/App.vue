d165b38df03c5d5aa08e665bf3802ad652b1b3ef
federicoverbicaro <federico.verbicaro@icloud.com>
2024-10-04 16:00:50 +0200

    start project

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="h-screen">
+    <div class="text-center w-full p-5">
+      <h1 class="text-red-500 text-4xl">Time Pomodoro</h1>
+    </div>
+
+    <div class="flex h-full justify-center items-center">
+      <div class="border border-black w-1/2 h-96 flex justify-center items-center">
+        <img class="h-80 w-80" src="../public/OIG4.jpeg" alt="">
+      </div>
+
+      <div class="border border-black w-1/2 h-96 flex justify-center items-center">
+        <div class="border border-black rounded-full h-96 w-96 flex justify-center items-center">
+          <span class="text-4xl">3:07</span>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+</template>
